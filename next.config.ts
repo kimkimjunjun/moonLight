752060380fda8976f495637a0b024985978d7ec1
kimkimjunjun <wnsgnl323@gmail.com>
2024-12-26 21:46:22 +0900

    fd

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: "http://223.130.150.200:4040/:path*"
+      },
+    ];
+  },
 };
 
 export default nextConfig;
