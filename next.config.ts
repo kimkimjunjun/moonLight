33e054fa48c7514f2c77f7d8c69068202a6c3bf8
kimkimjunjun <wnsgnl323@gmail.com>
2024-12-28 00:48:37 +0900

    fd

--- next.config.ts
+++ next.config.ts
@@ -9,10 +9,10 @@ const nextConfig: NextConfig = {
         source: "/api/:path*",
         destination: "http://223.130.137.39:6060/api/:path*"
       },
-      {
-        source: "/:path*",
-        destination: "http://localhost:8000/:path*"
-      },
+      // {
+      //   source: "/:path*",
+      //   destination: "http://223.130.150.200:4040/:path*"
+      // },
     ];
   },
 };
